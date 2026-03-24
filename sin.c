4614ae05ba7abb051dd9122ca3a77d3b03ac7459
cbgluca <cbgluca@gmail.com>
2026-03-24 23:06:10 +0100

    libraries

---
+++ sin.c
@@ -0,0 +1,15 @@
+//essenziali
+#include <stdio.h>
+#include <stdlib.h>
+#include<immintrin.h>
+#include<time.h>
+
+//aggiunte
+#include <string.h>
+#include <limits.h>
+#include <stdbool.h>
+
+//per confronto
+#include <math.h>
+
+
